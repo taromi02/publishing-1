88bbefbdfa8922185ac250748a5e4724860a7678
taromi02 <100741494+taromi02@users.noreply.github.com>
2022-04-10 18:40:14 +0900

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,22 +2,20 @@ import React from 'react';
 import { TextField } from '../components/TextField';
 
 export const LoginPage = () => {
-  return {
-    <div className="m-4">
-     <div className="text-3xl font-bold mb-10">로그인</div>
+  return (
+  <div className="m-4">
+    <div className="text-3xl font-bold mb-10">로그인</div>
 
-     <div>
+    <div>
       <TextField label="이메일" placeholder="이메일을 입력해주세요."/>
-      <TextField 
-       label="비밀번호" 
-       placeholder="비밀번호를 입력해주세요." 
-       type='password' />
-     </div>
+      <TextField label="비밀번호" 
+      placeholder="비밀번호를 입력해주세요." 
+      type="password"/>
+    </div>
 
-     <div className="mt-10">
+    <div>
       <div className="bg-gray-800 text-white text-center py-4">로그인</div>
       <div className="text-center py-4 text-gray-800 border-gray-800 mt-2">회원가입</div>
     </div>
-  </div>
-  );
-};
+  </div>);
+}
