4f2f2fbf7cd7e2231a3ac054f800f30da9a00ead
taromi02 <100741494+taromi02@users.noreply.github.com>
2022-04-10 23:33:39 +0900

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,9 +3,27 @@ import { TextField } from '../components/TextField';
 
 export const HomePage = () => {
   return (
-  <div className="border rounded-md">
-    <div className="text-3xl font-bold mb-7">List</div>
+  <div className="m-4">
+    <div>
+      <div>New</div>
+      <div></div>
+    </div>
 
+
+  <div>
+    <div className="text-3xl font-bold mb-7">List</div>
+    
+    <div className="border p-4">
+      <div>
+        <div className="h-48 bg-gray-100"></div>
+        <div className="mt-4">
+          <div className="text-lg font-semibold">Card title</div>
+          <div className="text-gray-500">
+            Sed vel turpis adipiscing penatibus orci meque.
+          </div>
+      </div>
+    
+    
     <img 
      src="http://source.unsplash.com/random"
      alt=""
@@ -20,4 +38,4 @@ export const HomePage = () => {
     </div>
 
   </div>);
-};
+}
